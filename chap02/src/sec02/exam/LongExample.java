58e30ad1742294d01e21a4d6cdabaaac33724bbe
someguy163 <wnguddl163@naver.com>
2024-01-03 17:12:20 +0900

    2024-01-03

---
+++ chap02/src/sec02/exam/LongExample.java
@@ -0,0 +1,18 @@
+package sec02.exam;
+
+public class LongExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		long var1 =10;
+		long var2 =20L;
+		//long var3 = 1000000000000;
+		long var4 = 5000000000000L;
+
+		System.out.println(var1);
+		System.out.println(var2);
+//		System.out.println(var3);
+		System.out.println(var4);
+	}
+
+}
