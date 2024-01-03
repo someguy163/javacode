a31ad5c4555b01d32074ddeb041ac89898ac15ff
someguy163 <wnguddl163@naver.com>
2024-01-03 17:07:32 +0900

    파일업로드

--- chap02/src/sec02/exam/FloatDoubleExam.java
+++ chap02/src/sec02/exam/FloatDoubleExam.java
@@ -4,7 +4,11 @@ public class FloatDoubleExam {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println();
+//		System.out.println("자바는");
+//		System.out.println("\n \"프로그래밍\"언어\n");
+//		System.out.println("입니다");
+		
+	
 	}
 
 }
---
+++ chap02/src/sec02/exam/IntegerLiteralExample.java
@@ -0,0 +1,18 @@
+package sec02.exam;
+
+public class IntegerLiteralExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int var1 = 0b1011;
+		int var2 = 0206;
+		int var3 = 365;
+		int var4 = 0xB3;
+
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var3);
+		System.out.println(var4);
+	}
+
+}
