b23d7f1b770490faf94a37cfe825b2e72beccabb
someguy163 <wnguddl163@naver.com>
2024-01-04 12:12:56 +0900

    ssss

--- chap02/src/sec04/exam/ScannerExample.java
+++ chap02/src/sec04/exam/ScannerExample.java
@@ -1,10 +1,24 @@
 package sec04.exam;
 
+import java.util.Scanner;
+
 public class ScannerExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner scanner = new Scanner(System.in);
+		String inputData;
+		
+		while (true) {
+			inputData = scanner.nextLine();
+			System.out.println("입력된 문자열 :\"" + inputData +"\"");
+			if (inputData.equals("q")) {
+				break;
+			}
+		}
+		System.out.println("종료");
+		scanner.close();
 	}
 
 }
