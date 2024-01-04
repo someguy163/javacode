502c6a3ce6936234ce33a54f57e86b9f03971d78
someguy163 <wnguddl163@naver.com>
2024-01-04 09:37:29 +0900

    2024-01-04

---
+++ chap02/src/sec02/exam/PromotionEx.java
@@ -0,0 +1,26 @@
+package sec02.exam;
+
+public class PromotionEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte byteValue = 10;
+		int intValue = byteValue;
+		System.out.println("intValue : "+ intValue);
+		
+		char charValue = '가';
+		intValue = charValue;
+		System.out.println("가의 유니코드 : " + intValue);
+		
+		intValue = 50;
+		float floatValue = intValue;
+		System.out.println("longValue : "+ floatValue);
+		
+		floatValue = 100.5F;
+		double doubleValue = floatValue;
+		System.out.println("floatValue : " + floatValue);
+		
+		
+	}
+
+}
