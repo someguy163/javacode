513a5259e8ab58ed3fd5fcb3522562ea590cd067
someguy163 <wnguddl163@naver.com>
2024-01-05 15:45:40 +0900

    랜덤숫자뽑기

---
+++ chap04/src/sec01/exam/Switch_랜덤숫자뽑기.java
@@ -0,0 +1,44 @@
+package sec01.exam;
+
+import java.util.Scanner;
+
+public class Switch_랜덤숫자뽑기 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		Scanner sc = new Scanner(System.in);
+		
+		int random = (int) (Math.random()*6+1);
+//		int random = (int) (Math.random()*6+1);
+		double random1 = Math.random()*6+1;
+		
+		
+		switch (random) {
+		case 1: 
+			System.out.println("1번이 나왔습니다");
+			break;
+		
+		case 2: 
+			System.out.println("2번이 나왔습니다");
+			break;
+		
+		case 3: 
+			System.out.println("3번이 나왔습니다");
+			break;
+		
+		case 4: 
+			System.out.println("4번이 나왔습니다");
+			break;
+		
+		case 5: 
+			System.out.println("5번이 나왔습니다");
+			break;
+		
+		default:
+			System.out.println("6");
+			break;
+//			throw new IllegalArgumentException("Unexpected value: " + key);
+		}
+	}
+
+}
