97782d455ceedc65860ac88008b40e97ded8e282
someguy163 <wnguddl163@naver.com>
2024-01-05 11:32:54 +0900

    2024-01-05

--- Quiz/src/Quiz/Quiz06_Scanner_각자릿수더하기.java
+++ Quiz/src/Quiz/Quiz06_Scanner_각자릿수더하기.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Quiz06_Scanner_각자릿수더하기 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+//		 TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 
 		System.out.print("0 ~ 999 사이의 숫자를 입력하세요 : ");
@@ -21,7 +21,10 @@ public class Quiz06_Scanner_각자릿수더하기 {
 //		1의 자리 출력하기
 //		System.out.println("result3 : " +result3 );
 		
+
 		System.out.println("각 자리수의 합 = " +(result + result2 + result3));
+		
+	
 	}
 
 }
