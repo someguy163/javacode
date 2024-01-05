7b796cb2b86451275fbe01418d1c6ce6970887db
someguy163 <wnguddl163@naver.com>
2024-01-05 15:51:52 +0900

    break 없는 Switch문

--- chap04/src/sec01/exam/Switch_랜덤시간출력.java
+++ chap04/src/sec01/exam/Switch_랜덤시간출력.java
@@ -8,18 +8,14 @@ public class Switch_랜덤시간출력 {
 		System.out.println("현재시간 : " +time);
 		switch (time) {
 		case 8: {
-			
 			System.out.println("출근합니다");
 		}
 		case 9: {
-			
 			System.out.println("회의를 갑니다");
 		}
 		case 10: {
-			
 			System.out.println("업무를 봅니다.");
 		}
-		
 		default:
 			System.out.println("외근을 갑니다");
 		}
