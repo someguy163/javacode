ee507813ca2c62d4be37535c23f6bf0d23c9737b
someguy163 <wnguddl163@naver.com>
2024-01-08 09:37:46 +0900

    for문

---
+++ chap04/src/반복문_for_while_do_While/for문.java
@@ -0,0 +1,13 @@
+package 반복문_for_while_do_While;
+
+public class for문 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub	
+		for(int i=0; i<=10; i++) {
+			System.out.println(i);
+		}
+	
+	}
+
+}
